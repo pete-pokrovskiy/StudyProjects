49d1f3ab844434b9d72462b65d02d740a0fde94b
pete <pokrovskiy.p@gmail.com>
2017-03-21 22:58:50 +0300

    Linked list.P1

---
+++ Study/Study.DataStructures/SimpleLinkedListNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.DataStructures
+{
+    public class SimpleLinkedListNode<T>
+    {
+        public SimpleLinkedListNode(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; set; }
+        public SimpleLinkedListNode<T> Next { get; set; }
+    }
+}

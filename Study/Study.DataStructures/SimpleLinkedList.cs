ef2370852337a352d2e71749e40ddb2eb92f9ecb
pete <pokrovskiy.p@gmail.com>
2017-03-22 16:59:42 +0300

    Связанный список. Функционал + первые тесты

--- Study/Study.DataStructures/SimpleLinkedList.cs
+++ Study/Study.DataStructures/SimpleLinkedList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Study.DataStructures
@@ -14,14 +15,75 @@ namespace Study.DataStructures
             Tail = null;
         } 
 
-        private SimpleLinkedListNode<T> Head { get; set; }
-        private SimpleLinkedListNode<T> Tail { get; set; }
+        public SimpleLinkedListNode<T> Head { get; set; }
+        public SimpleLinkedListNode<T> Tail { get; set; }
 
 
         public void AddFirst(T value)
         {
-            
+            AddFirst(new SimpleLinkedListNode<T>(value));   
         }
+
+        public void AddFirst(SimpleLinkedListNode<T> node)
+        {
+            if (Count == 0)
+            { 
+                Head = node;
+                Tail = node;
+                
+            }
+            else
+            {
+                node.Next = Tail;
+                Head = node;
+            }
+            Count++;
+
+        }
+
+        public void AddLast(T value)
+        {
+            AddLast(new SimpleLinkedListNode<T>(value));
+        }
+
+        public void AddLast(SimpleLinkedListNode<T> node)
+        {
+            if (Count == 0)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                SimpleLinkedListNode<T> temp = Tail;
+                temp.Next = node;
+                Tail = node;
+            }
+
+            Count++;
+        }
+
+        public void RemoveFirst()
+        {
+
+            if (Count == 0)
+                return;
+
+            if (Count == 1)
+            {
+                Head = null;
+                Tail = null;
+                Count--;
+                return;
+            }
+
+            Head = Head.Next;
+            Count--;
+
+        }
+
+
+        public int Count { get; set; }
 
 
 
---
+++ Study/Study.Tests/DataStructures/LinkedList/SimpleLinkedListTests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Study.DataStructures
+{
+    [TestFixture]
+    public class SimpleLinkedListTests
+    {
+        #region AddFirst
+
+        [Test]
+        public void AddFirstTestHead()
+        {
+            SimpleLinkedList<int> linkedList = new SimpleLinkedList<int>();
+
+            linkedList.AddFirst(5);
+            linkedList.AddFirst(4);
+            linkedList.AddFirst(3);
+            linkedList.AddFirst(2);
+            linkedList.AddFirst(1);
+            Assert.AreEqual(1, linkedList.Head.Value);
+            
+        }
+
+        [Test]
+        public void AddFirstTestTail()
+        {
+            SimpleLinkedList<int> linkedList = new SimpleLinkedList<int>();
+
+            linkedList.AddFirst(5);
+            linkedList.AddFirst(4);
+            linkedList.AddFirst(3);
+            linkedList.AddFirst(2);
+            linkedList.AddFirst(1);
+            Assert.AreEqual(5, linkedList.Tail.Value);
+        }
+
+        [Test]
+        public void AddFirstTestCount()
+        {
+            SimpleLinkedList<int> linkedList = new SimpleLinkedList<int>();
+
+            linkedList.AddFirst(5);
+            linkedList.AddFirst(4);
+            linkedList.AddFirst(3);
+            linkedList.AddFirst(2);
+            linkedList.AddFirst(1);
+            Assert.AreEqual(5, linkedList.Count);
+        }
+
+        #endregion
+
+        #region AddLast
+
+        [Test]
+        public void AddLastTestHead()
+        {
+            SimpleLinkedList<int> linkedList = new SimpleLinkedList<int>();
+
+            linkedList.AddLast(1);
+            linkedList.AddLast(2);
+            linkedList.AddLast(3);
+
+            Assert.AreEqual(1, linkedList.Head.Value);
+        }
+
+
+        [Test]
+        public void AddLastTestTail()
+        {
+            SimpleLinkedList<int> linkedList = new SimpleLinkedList<int>();
+
+            linkedList.AddLast(1);
+            linkedList.AddLast(2);
+            linkedList.AddLast(3);
+
+            Assert.AreEqual(3, linkedList.Tail.Value);
+        }
+
+        [Test]
+        public void AddLastTestCount()
+        {
+            SimpleLinkedList<int> linkedList = new SimpleLinkedList<int>();
+
+            linkedList.AddLast(1);
+            linkedList.AddLast(2);
+            linkedList.AddLast(3);
+            linkedList.AddLast(4);
+
+            Assert.AreEqual(4, linkedList.Count);
+        }
+
+        #endregion
+
+        #region RemoveFirst
+
+        [Test]
+        public void RemoveFirstTestOneNode()
+        {
+            //arrange
+            var sut = new SimpleLinkedList<int>();
+
+            //act
+            sut.AddFirst(4);
+            sut.RemoveFirst();
+            
+            //assert
+            Assert.That(sut.Tail, Is.Null);
+
+        }
+
+        [Test]
+        public void RemoveFirstTestSecond()
+        {
+            var sut = new SimpleLinkedList<int>();
+
+            sut.AddFirst(1);
+            sut.AddFirst(2);
+            sut.AddFirst(3);
+
+            sut.RemoveFirst();
+
+            Assert.That(sut.Head.Value, Is.EqualTo(2));
+
+        }
+
+#endregion
+    }
+}

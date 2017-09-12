1dc6e2c418134e9de34be7c2b09a968a89168181
pete <pokrovskiy.p@gmail.com>
2017-09-12 12:02:34 +0300

    Начало

---
+++ OwinKatanaStart/OwinKatanaStart/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace OwinKatanaStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "http://localhost:8080";
+
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Start");
+                Console.ReadKey();
+                Console.WriteLine("End");
+            }
+
+
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+
+            //app.Run(ctx => ctx.Response.WriteAsync("Heloo from console app!"));
+        }
+    }
+}
